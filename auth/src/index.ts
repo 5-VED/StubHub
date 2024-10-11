7104df69c9c1b79ac04df38fc62d1de7e2bd4f5f
5-VED <parmarved1@gmail.com>
2024-10-11 23:51:53 +0530

    Added mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,15 +8,40 @@ import { currentUserRouter } from "./routes/current-user"
 // Import Error Handaler
 import { errorHandler } from "./middlewares/error-handler"
 
-const app = express()
-app.use(express.json())
+//Import Cookie Session
+import cookieSession from 'cookie-session'
+import mongoose from "mongoose"
 
+const app = express()
+app.set('trust proxy', true)
+app.use(express.json())
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 app.use(signupRouter)
 app.use(signinRouter)
 app.use(currentUserRouter)
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-    console.log("Auth Service listeninig on port 3000")
-})
+const start = async () => {
+    console.log(process.env.JWT_KEY)
+    if (!process.env.JWT_KEY) {
+      throw new Error('JWT_KEY must be defined');
+    }
+  
+    // try {
+    //   await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  
+    //   console.log('Connected to MongoDb');
+    // } catch (err) {
+    //   console.error(err);
+    // }
+  
+    app.listen(3000, () => {
+      console.log('Listening on port 3000');
+    });
+  };
+
+  start()
